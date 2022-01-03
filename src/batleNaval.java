3b9dfe34af57977261594d06330094c363db3b0e
Isabel Alberti <isabelalberti84@gmail.com>
2022-01-03 17:22:28 -0300

    Finalizando a primeira parte - Adicionando posicionamento dos navios

--- src/batleNaval.java
+++ src/batleNaval.java
@@ -12,57 +12,57 @@ public class batleNaval {
 
 		int coluna;
 		int linha;
-		
+
 		System.out.print(
-				" ╔════╦════╦════╦════╦════╦════╦════╦════╦═══╗\n" + 
-				" ║    ║ 0  ║ 1  ║ 2  ║ 3  ║ 4  ║ 5  ║ 6  ║ 7 ║  \n" +
-				" ║    ╠════╬════╬════╬════╬════╬════╬════╬═══╣\n"
-				);
-				
+				  " ╔════╦════╦════╦════╦════╦════╦════╦════╦═══╗\n"
+				+ " ║    ║ 0  ║ 1  ║ 2  ║ 3  ║ 4  ║ 5  ║ 6  ║ 7 ║  \n"
+				+ " ║    ╠════╬════╬════╬════╬════╬════╬════╬═══╣\n");
+
 		for (linha = 0; linha < tabuleiro.length; linha++) {
 			System.out.print(" ║ " + " " + linha);
-		for (coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
-			tabuleiro[linha][coluna] = 0;
-			System.out.print(" ║ " + tabuleiro[linha][coluna] + " ");	
-				
+			for (coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
+				tabuleiro[linha][coluna] = 0;
+				System.out.print(" ║ " + tabuleiro[linha][coluna] + " ");
 			}
-		
+
 			System.out.print("║");
 			System.out.print("\n");
-			
 		}
-			System.out.print(" ╚════╩════╩════╩════╩════╩════╩════╩════╩═══╝");
-		
-	for(int contSub = 0; contSub <= 3; contSub++) {
-		System.out.println("\nEscolha a " + (contSub + 1) + "ª posição do seu Navio Submarino");
-		System.out.println("Linha");
-		linha =leia.nextInt();
-		System.out.println("Coluna");
-		coluna = leia.nextInt();
-		tabuleiro[linha][coluna] = 7;
+		System.out.print(" ╚════╩════╩════╩════╩════╩════╩════╩════╩═══╝");
+
+		for (int contSub = 0; contSub <= 4; contSub++) {
+			System.out.println("\nEscolha a " + (contSub + 1) + "ª posição do seu Navio Submarino");
+			System.out.println("Linha");
+			linha = leia.nextInt();
+			System.out.println("Coluna");
+			coluna = leia.nextInt();
+			
+			if (coluna >= tabuleiro.length || linha >= tabuleiro.length) {
+				System.out.println("Digite corretamente a posição!");
+				contSub--;
+			} else {			
+			tabuleiro[linha][coluna] = 7;
+		}
+			
 	}
+
+		System.out.print(
+				  " ╔════╦════╦════╦════╦════╦════╦════╦════╦═══╗\n"
+				+ " ║    ║ 0  ║ 1  ║ 2  ║ 3  ║ 4  ║ 5  ║ 6  ║ 7 ║  \n"
+				+ " ║    ╠════╬════╬════╬════╬════╬════╬════╬═══╣\n");
+
 		for (linha = 0; linha < tabuleiro.length; linha++) {
-			System.out.print(" " + linha);
-		for (coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
-			
-			System.out.print("|  " + tabuleiro[linha][coluna]);	
-		}
-		System.out.print("\n");
+			System.out.print(" ║ " + " " + linha);
+			for (coluna = 0; coluna < tabuleiro[linha].length; coluna++) {
+				System.out.print(" ║ " + tabuleiro[linha][coluna] + " ");
+			}
 
+			System.out.print("║");
+			System.out.print("\n");
 		}
-		
-		//String[][] campoDeBatalha = new String[8][8];
-       // System.out.println(
-              //  "\n------------------------------------------\n" +
-              //  "    CAMPO DE BATALHA\n"+
-              //  "  ║ 0 | 1 | 2 | 3 | 4 |\n" +
-               //         "═══════════════════════"
-       // );
+		System.out.print(" ╚════╩════╩════╩════╩════╩════╩════╩════╩═══╝");
 		
 		
 		
-	}	
-	
+	}
 }
-	
-
